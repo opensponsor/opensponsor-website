b4e5ee860771c7d762f974fe70b67e5aebc6f2c0
foxsir <contact@umbst.com>
2024-03-28 21:42:05 +0800

    add form example

---
+++ src/app/examples/form/form.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+    imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+    public formGroup = new FormGroup({
+        title: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(64),
+        ]),
+    });
+
+    public save() {
+        if (this.formGroup.valid) {
+            console.dir(this.formGroup);
+        }
+    }
+}
