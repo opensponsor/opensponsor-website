972abaf461f752149e3fb64e86605ed3e9f6f5d8
foxsir <contact@umbst.com>
2024-03-28 21:44:08 +0800

    add m3 theme

--- src/app/modules/organizations/new/new.component.ts
+++ src/app/modules/organizations/new/new.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     selector: 'app-new',
@@ -6,5 +7,17 @@ import {Component} from '@angular/core';
     styleUrl: './new.component.scss'
 })
 export class NewComponent {
+    public formGroup = new FormGroup({
+        title: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(64),
+        ]),
+    });
 
+    public save() {
+        if (this.formGroup.valid) {
+            console.dir(this.formGroup);
+        }
+    }
 }
--- src/app/modules/organizations/organizations.module.ts
+++ src/app/modules/organizations/organizations.module.ts
@@ -3,6 +3,19 @@ import {CommonModule} from '@angular/common';
 import {InfoComponent} from './info/info.component';
 import {NewComponent} from './new/new.component';
 import {RouterModule, Routes} from "@angular/router";
+import {
+    MatAnchor,
+    MatButton,
+    MatButtonModule,
+    MatFabButton,
+    MatIconButton,
+    MatMiniFabButton
+} from "@angular/material/button";
+import {MatDivider, MatDividerModule} from "@angular/material/divider";
+import {MatIcon, MatIconModule} from "@angular/material/icon";
+import {MatFormField, MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatInput, MatInputModule} from "@angular/material/input";
 
 const routes: Routes = [
     {
@@ -22,6 +35,12 @@ const routes: Routes = [
     imports: [
         CommonModule,
         RouterModule.forChild(routes),
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule
     ]
 })
 export class OrganizationsModule {
