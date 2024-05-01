db438885795f0534c11993887e53e7cc5414c744
foxsir <contact@umbst.com>
2024-05-01 17:12:20 +0800

    initialize user info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {AuthService} from "@services/auth/auth.service";
+import {Platform} from "@angular/cdk/platform";
 
 @Component({
     selector: 'app-root',
@@ -10,4 +12,19 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
     title = 'opensponsor-website';
+
+    constructor(
+        private readonly authService: AuthService,
+        public readonly platform: Platform,
+    ) {
+        if (this.platform.isBrowser) {
+            this.authService.getAuthUser().subscribe(res => {
+                if(res.body) {
+                    this.authService.persistAuth(res.body);
+                } else {
+                    this.authService.removeAuthInfo();
+                }
+            })
+        }
+    }
 }
