b6f0600f0e084786b3739cadc72eda6c6f3af343
foxsir <contact@umbst.com>
2024-05-01 17:02:12 +0800

    update login page

--- src/app/modules/passport/login/login.component.ts
+++ src/app/modules/passport/login/login.component.ts
@@ -1,4 +1,9 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "@services/auth/auth.service";
+import {User} from "@app/interfaces/ApiInterface";
+import {Router} from "@angular/router";
+import {SnackBarService} from "@services/snack-bar/snack-bar.service";
 
 @Component({
     selector: 'app-login',
@@ -6,5 +11,40 @@ import {Component} from '@angular/core';
     styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    constructor(
+        private readonly authService: AuthService,
+        private readonly router: Router,
+        private readonly snackBarService: SnackBarService,
+    ) {
+    }
 
+    public formGroup = new FormGroup({
+        account: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(64),
+        ]),
+        password: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(4),
+            Validators.maxLength(64),
+        ]),
+    });
+
+    public login() {
+        if (this.formGroup.valid) {
+            const data = {
+                account: String(this.formGroup.value.account),
+                password: String(this.formGroup.value.password),
+            }
+            this.authService.login<User>(data).subscribe(res => {
+                if(res.body) {
+                    this.authService.persistAuth(res.body);
+                    this.router.navigateByUrl("/").then(() => {
+                        this.snackBarService.message({message: 'Login successful!'})
+                    });
+                }
+            })
+        }
+    }
 }
