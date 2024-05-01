ce837284fa3684425948eb01b654dc4fd10c2260
foxsir <contact@umbst.com>
2024-05-01 17:12:29 +0800

    update auth service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -5,19 +5,21 @@ import RequestSignIn from "@app/payload/RequestSignIn";
 import RequestSignInUseCode from "@app/payload/RequestSignInUseCode";
 import {toObservable} from '@angular/core/rxjs-interop';
 import AuthInfo from "@app/models/AuthInfo";
+import {User} from "@app/interfaces/ApiInterface";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
     private Urls = {
-        signIn: "/auth/sign-in",
-        signInUseCode: "/auth/sign-in-sms",
-        signUp: "/auth/sign-up",
-        signOut: "/auth/sign-out",
+        login: "/session/login",
+        loginForCode: "/session/login-for-sms",
+        register: "/session/register",
+        logout: "/session/logout",
+        user: "/session/user",
     }
 
-    authInfo = signal<AuthInfo | null>(null);
+    authInfo = signal<User | null>(null);
 
     authInfo$ = toObservable(this.authInfo);
 
@@ -26,34 +28,38 @@ export class AuthService {
     ) {
     }
 
-    public signIn<T>(data: RequestSignIn) {
-        return this.httpService.post<T>(this.Urls.signIn, data);
+    public login<T>(data: RequestSignIn) {
+        return this.httpService.post<T>(this.Urls.login, data);
     }
 
-    public signInUseCode<T>(data: RequestSignInUseCode) {
-        return this.httpService.post<T>(this.Urls.signInUseCode, data);
+    public loginForCode<T>(data: RequestSignInUseCode) {
+        return this.httpService.post<T>(this.Urls.loginForCode, data);
     }
 
-    public signUp<T>(data: RequestSignUp) {
-        return this.httpService.post<T>(this.Urls.signUp, data);
+    public register<T>(data: RequestSignUp) {
+        return this.httpService.post<T>(this.Urls.register, data);
     }
 
-    public signOut<T>(data: any) {
-        return this.httpService.post<T>(this.Urls.signOut, data);
+    public logout<T>(data: any) {
+        return this.httpService.post<T>(this.Urls.logout, data);
     }
 
     public resetPassword() {
     }
 
-    public saveAuthInfo(authInfo: AuthInfo | null) {
-        if (authInfo) {
-            localStorage.setItem("accessToken", authInfo.accessToken);
-            this.authInfo.set(authInfo);
+    public persistAuth(user: User) {
+        if (user && user.token?.token) {
+            localStorage.setItem("accessToken", user.token.token);
+            this.authInfo.set(user);
         }
     }
 
     public removeAuthInfo() {
         localStorage.removeItem("accessToken");
         this.authInfo.set(null);
+    }
+
+    public getAuthUser() {
+        return this.httpService.get<User>(this.Urls.user);
     }
 }
