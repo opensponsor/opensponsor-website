66f61110a7bbab287c9ad4732d4ca82a719e0d1a
foxsir <contact@umbst.com>
2024-05-14 22:12:19 +0800

    update ApiInterface.ts

--- src/app/interfaces/ApiInterface.ts
+++ src/app/interfaces/ApiInterface.ts
@@ -51,6 +51,189 @@ export interface CountryCodes {
   whenDeleted?: Instant;
 }
 
+export enum E_AMOUNT_TYPE {
+  FIXED = "FIXED",
+  FLEXIBLE = "FLEXIBLE",
+}
+
+export enum E_IBAN_CURRENCIES {
+  AED = "AED",
+  AFN = "AFN",
+  ALL = "ALL",
+  AMD = "AMD",
+  ANG = "ANG",
+  AOA = "AOA",
+  ARS = "ARS",
+  AUD = "AUD",
+  AWG = "AWG",
+  AZN = "AZN",
+  BAM = "BAM",
+  BBD = "BBD",
+  BDT = "BDT",
+  BGN = "BGN",
+  BHD = "BHD",
+  BIF = "BIF",
+  BMD = "BMD",
+  BND = "BND",
+  BOB = "BOB",
+  BOV = "BOV",
+  BRL = "BRL",
+  BSD = "BSD",
+  BTN = "BTN",
+  BWP = "BWP",
+  BYN = "BYN",
+  BZD = "BZD",
+  CAD = "CAD",
+  CDF = "CDF",
+  CHE = "CHE",
+  CHF = "CHF",
+  CHW = "CHW",
+  CLF = "CLF",
+  CLP = "CLP",
+  CNY = "CNY",
+  COP = "COP",
+  COU = "COU",
+  CRC = "CRC",
+  CUC = "CUC",
+  CUP = "CUP",
+  CVE = "CVE",
+  CZK = "CZK",
+  DJF = "DJF",
+  DKK = "DKK",
+  DOP = "DOP",
+  DZD = "DZD",
+  EGP = "EGP",
+  ERN = "ERN",
+  ETB = "ETB",
+  EUR = "EUR",
+  FJD = "FJD",
+  FKP = "FKP",
+  GBP = "GBP",
+  GEL = "GEL",
+  GHS = "GHS",
+  GIP = "GIP",
+  GMD = "GMD",
+  GNF = "GNF",
+  GTQ = "GTQ",
+  GYD = "GYD",
+  HKD = "HKD",
+  HNL = "HNL",
+  HRK = "HRK",
+  HTG = "HTG",
+  HUF = "HUF",
+  IDR = "IDR",
+  ILS = "ILS",
+  INR = "INR",
+  IQD = "IQD",
+  IRR = "IRR",
+  ISK = "ISK",
+  JMD = "JMD",
+  JOD = "JOD",
+  JPY = "JPY",
+  KES = "KES",
+  KGS = "KGS",
+  KHR = "KHR",
+  KMF = "KMF",
+  KPW = "KPW",
+  KRW = "KRW",
+  KWD = "KWD",
+  KYD = "KYD",
+  KZT = "KZT",
+  LAK = "LAK",
+  LBP = "LBP",
+  LKR = "LKR",
+  LRD = "LRD",
+  LSL = "LSL",
+  LYD = "LYD",
+  MAD = "MAD",
+  MDL = "MDL",
+  MGA = "MGA",
+  MKD = "MKD",
+  MMK = "MMK",
+  MNT = "MNT",
+  MOP = "MOP",
+  MRU = "MRU",
+  MUR = "MUR",
+  MVR = "MVR",
+  MWK = "MWK",
+  MXN = "MXN",
+  MXV = "MXV",
+  MYR = "MYR",
+  MZN = "MZN",
+  NAD = "NAD",
+  NGN = "NGN",
+  NIO = "NIO",
+  NOK = "NOK",
+  NPR = "NPR",
+  NZD = "NZD",
+  OMR = "OMR",
+  PAB = "PAB",
+  PEN = "PEN",
+  PGK = "PGK",
+  PHP = "PHP",
+  PKR = "PKR",
+  PLN = "PLN",
+  PYG = "PYG",
+  QAR = "QAR",
+  RON = "RON",
+  RSD = "RSD",
+  RUB = "RUB",
+  RWF = "RWF",
+  SAR = "SAR",
+  SBD = "SBD",
+  SCR = "SCR",
+  SDG = "SDG",
+  SEK = "SEK",
+  SGD = "SGD",
+  SHP = "SHP",
+  SLL = "SLL",
+  SOS = "SOS",
+  SRD = "SRD",
+  SSP = "SSP",
+  STN = "STN",
+  SVC = "SVC",
+  SYP = "SYP",
+  SZL = "SZL",
+  THB = "THB",
+  TJS = "TJS",
+  TMT = "TMT",
+  TND = "TND",
+  TOP = "TOP",
+  TRY = "TRY",
+  TTD = "TTD",
+  TWD = "TWD",
+  TZS = "TZS",
+  UAH = "UAH",
+  UGX = "UGX",
+  USD = "USD",
+  USN = "USN",
+  UYI = "UYI",
+  UYU = "UYU",
+  UZS = "UZS",
+  VEF = "VEF",
+  VND = "VND",
+  VUV = "VUV",
+  WST = "WST",
+  XAF = "XAF",
+  XCD = "XCD",
+  XDR = "XDR",
+  XOF = "XOF",
+  XPF = "XPF",
+  XSU = "XSU",
+  XUA = "XUA",
+  YER = "YER",
+  ZAR = "ZAR",
+  ZMW = "ZMW",
+  ZWL = "ZWL",
+}
+
+export enum E_INTERVAL {
+  ONETIME = "ONETIME",
+  MONTH = "MONTH",
+  YEAR = "YEAR",
+  FLEXIBLE = "FLEXIBLE",
+}
+
 export enum E_ORGANIZATION_TYPE {
   FISCAL_HOST = "FISCAL_HOST",
   COMMUNITY = "COMMUNITY",
@@ -59,6 +242,14 @@ export enum E_ORGANIZATION_TYPE {
 export enum E_SEX {
   WOMEN = "WOMEN",
   MAN = "MAN",
+}
+
+export enum E_TIER_TYPE {
+  GENERIC = "GENERIC",
+  MEMBERSHIP = "MEMBERSHIP",
+  DONATION = "DONATION",
+  PRODUCT = "PRODUCT",
+  SERVICE = "SERVICE",
 }
 
 /**
@@ -109,6 +300,8 @@ export interface Organization {
    * @maxLength 32
    */
   website?: string;
+  /** 捐助等级 */
+  tiers?: Tier[];
   /** Organization Owner */
   user?: User;
   whenCreated?: Instant;
@@ -141,6 +334,61 @@ export interface ResteasyConstraintViolation {
   path?: string;
   message?: string;
   value?: string;
+}
+
+export interface Tier {
+  id?: UUID;
+  /** 所属组织 */
+  organization?: Organization;
+  /**
+   * url path
+   * @minLength 2
+   * @maxLength 32
+   */
+  slug?: string;
+  /**
+   * 捐助等级名称
+   * @minLength 2
+   * @maxLength 32
+   */
+  name?: string;
+  /** 捐助等级类型 */
+  type?: E_TIER_TYPE;
+  /**
+   * 捐助等级类型
+   * @maxLength 500
+   */
+  description?: string;
+  /** 捐助等级类型 */
+  longDescription?: string;
+  /** 是否使用独立页面 */
+  useStandalonePage?: boolean;
+  /** 视频URL */
+  videoUrl?: string;
+  /**
+   * 按钮文字
+   * @maxLength 16
+   */
+  button?: string;
+  /** 捐助金额 */
+  amount?: number;
+  /** 捐助金额范围 */
+  presets?: number[];
+  /** 捐助金额类型 */
+  amountType?: E_AMOUNT_TYPE;
+  /** 最小金额 */
+  minimumAmount?: number;
+  /** 最小金额 */
+  currency?: E_IBAN_CURRENCIES;
+  /** 捐助方式 */
+  interval?: E_INTERVAL;
+  /** 库存 */
+  maxQuantity?: number;
+  /** 筹款目标 */
+  goal?: number;
+  whenCreated?: Instant;
+  whenModified?: Instant;
+  whenDeleted?: Instant;
 }
 
 export enum Type {
