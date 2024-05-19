7581f65658fe6d8202f885a2df8b38fb4ea3ec90
foxsir <contact@umbst.com>
2024-05-19 21:21:35 +0800

    tier service

---
+++ src/app/services/tier/tier.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {HttpService} from "@services/http/http.service";
+import {Tier} from "@app/interfaces/ApiInterface";
+import {HttpParams} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TierService {
+
+    private Urls = {
+        create: "/tier/create",
+        list: "/edit",
+    }
+
+    constructor(private readonly httpService: HttpService) {
+    }
+
+    public create(tier: Tier) {
+        return this.httpService.post<Tier>(this.Urls.create, tier);
+    }
+
+    public edit(tier: Tier) {
+        return this.httpService.put<Tier>(this.Urls.create, tier);
+    }
+
+    public delete(tier: Tier) {
+        const params = new HttpParams();
+        params.set('id', String(tier.id))
+        return this.httpService.delete(this.Urls.create, params);
+    }
+
+
+}
