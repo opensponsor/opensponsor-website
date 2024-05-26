5f37f3ee20a3c5fbfd3b0f72d82b6046b99da6e7
foxsir <contact@umbst.com>
2024-05-26 20:55:16 +0800

    add info page

--- src/app/modules/organizations/info/info.component.ts
+++ src/app/modules/organizations/info/info.component.ts
@@ -1,4 +1,8 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {OrganizationsService} from "@services/organizations/organizations.service";
+import {Platform} from "@angular/cdk/platform";
+import {Organization} from "@app/interfaces/ApiInterface";
 
 @Component({
     selector: 'app-info',
@@ -6,5 +10,27 @@ import {Component} from '@angular/core';
     styleUrl: './info.component.scss'
 })
 export class InfoComponent {
+    public organization: Partial<Organization> = {};
 
+    constructor(
+        private readonly router: Router,
+        private readonly activatedRoute: ActivatedRoute,
+        private readonly organizationsService: OrganizationsService,
+        private readonly platform: Platform,
+    ) {
+        if(this.platform.isBrowser) {
+            this.getOrg();
+        }
+    }
+
+    private getOrg() {
+        const name = this.activatedRoute.snapshot.paramMap.get("name") as string;
+        this.organizationsService.getOrganizationByName(this.activatedRoute.snapshot.paramMap.get("name") as string).subscribe((res) => {
+            if(res.body) {
+                this.organization = res.body;
+            } else {
+                // not found org
+            }
+        })
+    }
 }
