cfa4ed835ef18ebe1ea4e9894999c9fdd0dc9c81
foxsir <contact@umbst.com>
2024-06-30 19:43:52 +0800

    error module

---
+++ src/app/modules/error/error.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {path: '', component: PageNotFoundComponent, pathMatch: 'full'},
+        ]
+    },
+];
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forChild(routes)
+    ]
+})
+export class ErrorModule {
+}
