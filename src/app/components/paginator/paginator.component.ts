766405cbe7c632862c435099f38acc787a2f53a3
foxsir <contact@umbst.com>
2024-07-21 17:20:32 +0800

    add paginator component

---
+++ src/app/components/paginator/paginator.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input} from '@angular/core';
+import {HttpResult} from "@services/http/http.service";
+import {MatPaginator} from "@angular/material/paginator";
+
+@Component({
+  selector: 'app-paginator',
+  standalone: true,
+    imports: [
+        MatPaginator
+    ],
+  templateUrl: './paginator.component.html',
+  styleUrl: './paginator.component.scss'
+})
+export class PaginatorComponent {
+    @Input({
+        required: true,
+    })
+    result: HttpResult<any> | undefined;
+}
