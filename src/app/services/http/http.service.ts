6ca7652be5afd7d61f8692d691cf66910098e4f9
foxsir <838394225@qq.com>
2025-02-02 18:35:44 +0800

    chore: modify error message

--- src/app/services/http/http.service.ts
+++ src/app/services/http/http.service.ts
@@ -69,8 +69,7 @@ export abstract class HttpService {
       }
     } else if (res.status === 401) {
       localStorage.removeItem("accessToken")
-      // this.snackBar.message({message: "登录信息失效, 请先登录."});
-      return throwError(() => new Error('请先登陆.'));
+      return throwError(() => new Error('请先登录。'));
     } else {
       this.snackBar.message({message: res.message});
     }
