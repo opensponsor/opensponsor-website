f25c0656fc14f8acabee1bce85d271bc43b03252
foxsir <838394225@qq.com>
2025-02-03 14:08:06 +0800

    feat: update generic type

--- src/app/services/sms/sms.service.ts
+++ src/app/services/sms/sms.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpService} from "@services/http/http.service";
-import {CountryCode} from "@app/interfaces/ApiInterface";
+import {CountryCode, SendSmsResponseBody} from "@app/interfaces/ApiInterface";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +9,6 @@ export class SmsService {
   constructor(private httpService: HttpService) { }
 
   sendVerificationCode(countryCode: CountryCode, phoneNumber: string) {
-    return this.httpService.post("/sms/verifyCode", {countryCode: countryCode, phoneNumber: phoneNumber})
+    return this.httpService.post<SendSmsResponseBody>("/sms/verifyCode", {countryCode: countryCode, phoneNumber: phoneNumber})
   }
 }
