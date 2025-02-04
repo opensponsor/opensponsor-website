9518faf45c12e4ad0a683d4dfad46592572dbffa
foxsir <838394225@qq.com>
2025-02-04 16:55:05 +0800

    feat: remove

--- src/app/payload/RequestLogin.ts
+++
@@ -1,5 +0,0 @@
-export default interface RequestLogin {
-  account: string;
-
-  password: string;
-}
--- src/app/payload/RequestSignInUseCode.ts
+++
@@ -1,5 +0,0 @@
-export default interface RequestSignInUseCode {
-  mobile: string;
-
-  code: string;
-}
