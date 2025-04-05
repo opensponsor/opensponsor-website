b57104d15ff3a02c7adc2c288305f3c4182138cf
foxsir <838394225@qq.com>
2025-04-05 15:56:33 +0800

    update pattern type

--- src/app/services/dialog/dialog.service.ts
+++ src/app/services/dialog/dialog.service.ts
@@ -20,31 +20,31 @@ export class DialogService {
     maxWidth: "80vw",
   }
 
-  public open<T, R>(component: ComponentType<any>, config?: MatDialogConfig) {
-    return this.dialog.open<T, R>(component, {
+  public open<T, D, R>(component: ComponentType<any>, config?: MatDialogConfig) {
+    return this.dialog.open<T, D, R>(component, {
       ...this.defaultConfig,
       ...config,
     });
   }
 
-  public openXS<T, R>(component: ComponentType<any>, config?: MatDialogConfig) {
-    return this.open<T, R>(component, {...{width: "30vw"}, ...config});
+  public openXS<T, D, R>(component: ComponentType<any>, config?: MatDialogConfig) {
+    return this.open<T, D, R>(component, {...{width: "30vw"}, ...config});
   }
 
-  public openMD<T, R>(component: ComponentType<any>, config?: MatDialogConfig) {
-    return this.open<T, R>(component, {...{width: "60vw"}, ...config});
+  public openMD<T, D, R>(component: ComponentType<any>, config?: MatDialogConfig) {
+    return this.open<T, D, R>(component, {...{width: "60vw"}, ...config});
   }
 
-  public openLG<T, R>(component: ComponentType<any>, config?: MatDialogConfig) {
-    return this.open<T, R>(component, {...{width: "80vw"}, ...config});
+  public openLG<T, D, R>(component: ComponentType<any>, config?: MatDialogConfig) {
+    return this.open<T, D, R>(component, {...{width: "80vw"}, ...config});
   }
 
   public alert(alertOptions: AlertOptions) {
-    return this.openXS(AlertComponent, {data: alertOptions, disableClose: true});
+    return this.openXS<any, any, boolean>(AlertComponent, {data: alertOptions, disableClose: true});
   }
 
   public confirm(confirmOptions: ConfirmOptions) {
-    return this.openXS(ConfirmComponent, {data: confirmOptions, disableClose: true});
+    return this.openXS<any, any, boolean>(ConfirmComponent, {data: confirmOptions, disableClose: true});
   }
 
 }
