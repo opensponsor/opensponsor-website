d62bc05aa3f59b7ca6a2a31054f65326b3194761
foxsir <838394225@qq.com>
2025-05-17 15:53:56 +0800

    add form field country-select

---
+++ src/app/forms/country-select/country-select.component.ts
@@ -0,0 +1,66 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatOption} from "@angular/material/core";
+import {MatSelect} from "@angular/material/select";
+import {NgForOf} from "@angular/common";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {CountryCodesService} from "@services/countryCodes/country-codes.service";
+import {CountryCode} from "@app/interfaces/ApiInterface";
+import {enumTranslate} from "@app/languages/zh_cn/enumTranslate";
+
+@Component({
+  selector: 'os-country-select',
+  imports: [
+    MatFormField,
+    MatOption,
+    MatSelect,
+    NgForOf,
+    ReactiveFormsModule,
+    MatLabel,
+  ],
+  templateUrl: './country-select.component.html',
+  styleUrl: './country-select.component.scss'
+})
+export class CountrySelectComponent implements OnInit {
+  @Input({
+    required: true,
+  })
+  countryCodeControl!: FormControl;
+
+  @Input({
+    required: false,
+  })
+  required: boolean = false;
+
+  @Input({
+    required: true,
+  })
+  label!: string;
+
+  @Input({
+    required: false,
+  })
+  labelType: 'officialNameCn' | 'currencyAlphabeticCode' = 'officialNameCn';
+
+  public countryCodes: CountryCode[] = [];
+
+  constructor(
+    private readonly countryCodesService: CountryCodesService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.getAllCountryCodes();
+  }
+
+  private getAllCountryCodes() {
+    this.countryCodesService.all().then(codes => {
+      this.countryCodes = codes;
+      if (this.countryCodes.length > 0) {
+        this.countryCodeControl.setValue(this.countryCodes[0] as any);
+      }
+    })
+  }
+
+  protected readonly enumTranslate = enumTranslate;
+}
