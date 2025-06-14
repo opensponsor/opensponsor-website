700c0d647979e44b773751967befa5c4a8136316
foxsir <838394225@qq.com>
2025-06-14 12:57:21 +0800

    update @angular/material

--- src/app/modules/dashboard/components/dashboard-container/dashboard-container.component.ts
+++ src/app/modules/dashboard/components/dashboard-container/dashboard-container.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'os-dashboard-container',
   imports: [
-    RouterOutlet
   ],
   templateUrl: './dashboard-container.component.html',
   styleUrl: './dashboard-container.component.scss'
