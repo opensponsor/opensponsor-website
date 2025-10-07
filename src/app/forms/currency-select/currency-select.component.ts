bd358ffb4316644cad0eae9595b8a8cb8bc71a6e
foxsir <838394225@qq.com>
2025-10-07 17:14:59 +0800

    add currency-select

---
+++ src/app/forms/currency-select/currency-select.component.ts
@@ -0,0 +1,42 @@
+import {Component, Input} from '@angular/core';
+import {E_IBAN_CURRENCIES} from "@app/interfaces/ApiInterface";
+import {MatFormField, MatHint, MatLabel} from "@angular/material/form-field";
+import {MatOption} from "@angular/material/core";
+import {MatSelect} from "@angular/material/select";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {TranslatePipe} from "@app/pipes/translate/translate.pipe";
+
+@Component({
+  selector: 'os-currency-select',
+  imports: [
+    MatFormField,
+    MatOption,
+    MatSelect,
+    ReactiveFormsModule,
+    MatLabel,
+    MatHint,
+    TranslatePipe,
+  ],
+  templateUrl: './currency-select.component.html',
+  styleUrl: './currency-select.component.scss'
+})
+export class CurrencySelectComponent {
+  @Input({
+    required: true,
+  })
+  currencyControl!: FormControl;
+
+  @Input({
+    required: false,
+  })
+  required: boolean = false;
+
+  @Input({
+    required: true,
+  })
+  label!: string;
+
+  protected readonly E_IBAN_CURRENCIES = E_IBAN_CURRENCIES;
+  protected readonly Object = Object;
+  protected readonly console = console;
+}
