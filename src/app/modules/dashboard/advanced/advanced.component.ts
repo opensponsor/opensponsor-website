3630477d47012014dbed7c60df213b3ba12e6579
foxsir <838394225@qq.com>
2025-10-07 21:55:01 +0800

    add advanced.component

---
+++ src/app/modules/dashboard/advanced/advanced.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'os-advanced',
+  imports: [],
+  templateUrl: './advanced.component.html',
+  styleUrl: './advanced.component.scss'
+})
+export class AdvancedComponent {
+
+}
