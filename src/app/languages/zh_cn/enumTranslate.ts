06ed837a238a23cecd9868f06c3734ccac9dc432
foxsir <838394225@qq.com>
2025-11-29 16:03:15 +0800

    update enumTranslate

--- src/app/languages/zh_cn/enumTranslate.ts
+++ src/app/languages/zh_cn/enumTranslate.ts
@@ -1,7 +1,7 @@
 import {
   E_AMOUNT_TYPE,
   E_IBAN_CURRENCIES,
-  E_INTERVAL,
+  E_INTERVAL, E_ORGANIZATION_TYPE,
   E_PAYMENT_METHOD,
   E_SEX,
   E_TIER_TYPE
@@ -15,7 +15,6 @@ export const enumTranslate: Record<string, string> = {
 
   [E_AMOUNT_TYPE.FIXED]: "固定金额",
   // [E_AMOUNT_TYPE.FLEXIBLE]: "弹性的",
-
 
   [E_IBAN_CURRENCIES.CNY]: "人民币",
   [E_IBAN_CURRENCIES.USD]: "美元",
@@ -31,4 +30,10 @@ export const enumTranslate: Record<string, string> = {
   [E_SEX.MAN]: "男性",
   [E_PAYMENT_METHOD.WE_CHAT_PAY]: "微信支付",
   [E_PAYMENT_METHOD.ALI_PAY]: "支付宝支付",
+
+  [E_ORGANIZATION_TYPE.ORGANIZATION]: "组织",
+  [E_ORGANIZATION_TYPE.COMMUNITY]: "社区",
+  [E_ORGANIZATION_TYPE.FISCAL_HOST]: "财务托管方",
+  [E_ORGANIZATION_TYPE.EVENT]: "事件",
+  [E_ORGANIZATION_TYPE.PROJECT]: "项目",
 }
